7ad9054e397de41d4cbc13a2be851af28d48c58a
xuan hoang <hoang24k69@gmail.com>
2024-05-15 10:44:50 +0700

    lesson 2

--- lesson_1_basic/Program.cs
+++ lesson_1_basic/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,42 @@ namespace lesson_1_basic
     {
         static void Main(string[] args)
         {
+            /*
             //viet tat cw
             Console.WriteLine();
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            string a = x > 10 ? "lon hon 10" : "nho hon hoac bang 10";
+            Console.WriteLine(a);
+
+            Console.WriteLine(x);
+            //{0:0.00}: doc phan tu thu 0 sau dau , ; phan tu thu 0 co dinh dang: giu nguyen phan so nguyen, 2 chu so thap phan
+            Console.WriteLine(string.Format("{0:0.00}", x));
+
+            //"c" dinh dang kieu tien, lay 2 chu so phan thap phan
+            Console.WriteLine(x.ToString("C"));
+
+            //co gang chuyen kieu sang double, neu chuyen duoc thi x nhan gia tri moi, neu khong thi x giu gia tri cu
+            double.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine(x);
+            */
+
+            //ex1:
+            Console.Write("enter a int number: ");
+            int a = 0;
+            if (int.TryParse(Console.ReadLine(), out a))
+            {
+                for(int i = 0; i < a; i++)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            else
+            {
+
+            }
+
+
         }
     }
 }
